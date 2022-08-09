cbe0cd2c65fd7051052fa4a800c251a76886b2ab
AMan Chaudhary <achoudhary@deqode.com>
2022-08-09 15:34:37 +0530

    chore: minor changes

--- src/consumer/hash.consumer.ts
+++ src/consumer/hash.consumer.ts
@@ -3,6 +3,7 @@ import { HashService } from "../services/hash.service";
 
 async function hashConsumer(job: Job) {
   try {
+    console.log("job", job.data.status);
     await new HashService().findingNonce(job);
   } catch (error) {
     throw new Error(error.message);
